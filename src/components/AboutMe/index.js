63a734f4e1c87551cdd9dca9b70573e8955c2789
Brian Anderson <banders6@1800contacts.com>
2022-03-15 06:24:20 -0600

    Getting ready to deploy.

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 function AboutMe() {
 
 	return (
 		<div>
 			<div className="flex">
-				<img src="202.jpeg" width="320" height="240" />
+				<img src="202.jpeg" width="320" height="240" alt="Brian and Lyndsay" />
 				<div className="mx-2">
 					<p className="my-2">Hi! I'm Brian. I live with my wife, three kids, dog, and cat in Sandy, Utah.</p>
 					<p className="my-2">I enjoy electronics, robotics, IoT household automation, and bike rides with my son and dog.</p>
