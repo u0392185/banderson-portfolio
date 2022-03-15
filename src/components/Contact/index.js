e13af4e72cbc8a21b99fd62969149d4c3376440c
Brian Anderson <banders6@1800contacts.com>
2022-03-15 04:02:54 -0600

    The contact form is working.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Contact() {
+function ContactForm() {
+	
+	const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+	const { name, email, message } = formState;
+
+	const [errorMessage, setErrorMessage] = useState('');
+
+	function handleChange(e) {
+		if(e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value)
+			console.log(isValid)
+			// isValid conditional statement
+			if (!isValid) {
+			  setErrorMessage('Please enter a valid email.');
+			} else {
+			  setErrorMessage('');
+			}
+		} else {
+			if(!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage('');
+			}
+		}
+		if(!errorMessage) {
+			setFormState({...formState, [e.target.name]: e.target.value})
+		}
+
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		validateEmail()
+		console.log(formState)
+	}
+
+	function validateEmail(email) {
+		console.log('The email: ', email)
+
+		return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+	}
 
 	return (
-		<div>
-			Contact
-		</div>
+		<section>
+			<h1 data-testid="" className="text-2xl text-teal-600 mb-6">Send me a message</h1>
+			<form id="contact-form" onSubmit={handleSubmit} className="text-teal-100">
+		        <div className="flex my-2">
+		        	<div className="w-1/4">
+		        		<label  htmlFor="name">Name:</label>
+		        	</div>
+		        	<div className="w-1/2">
+				  		<input className="text-black" type="text" name="name" defaultValue={name} onBlur={handleChange} />
+		        	</div>		
+				</div>
+				<div className="flex my-2">
+		        	<div className="w-1/4">
+		        		<label htmlFor="email">Email address:</label>
+		        	</div>
+		        	<div className="w-1/2">
+				  		<input className="text-black" type="email" name="email" defaultValue={email} onBlur={handleChange} />
+		        	</div>		        									  
+				</div>
+				<div className="flex my-2">
+					<div className="w-1/4">
+				  		<label htmlFor="message">Message:</label>
+					</div>	
+					<div className="w-1/2">
+				  		<textarea className="text-black" name="message" rows="5"  defaultValue={message} onBlur={handleChange} />
+					</div>	
+				</div>
+				{errorMessage && (
+					<div>
+						<p className="error-text text-red-400 my-2">{errorMessage}</p>
+					</div>
+				)}
+				<button 
+					className="btn hover:bg-blue-500 text-teal-100 bg-teal-500 font-bold py-2 px-4 border-blue-700 rounded" 
+					data-testid="submitButton" 
+					type="submit"
+				>Submit</button>
+			</form>
+		</section>
 	)
 }
 
-export default Contact
+export default ContactForm
